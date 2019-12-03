5449dcb268a6e5718d2c1965e2f24a3d5c838fe1
Bulat Mendagaziev <bm@idaproject.com>
2019-12-04 00:51:09 +0300

    refactor(HomeScreen): update template and test

--- src/home-screen/home-screen.test.js
+++ src/home-screen/home-screen.test.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
-import HomeScreen from './HomeScreen';
+import {HomeScreen} from './HomeScreen';
+import {offers} from '../mocks/offers';
 
 describe(`HomeScreen`, () => {
   it(`renders correctly`, () => {
     const tree = renderer
-      .create(<HomeScreen/>)
+      .create(<HomeScreen offers={offers} />)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
-  });
-
-  it(`renders offers`, () => {
-    const offers = [
-      `Beautiful & luxurious apartment at great location`,
-      `Wood and stone place`,
-      `Canal View Prinsengracht`,
-    ];
-
-    const app = shallow(<HomeScreen offers={offers}/>);
-
-    expect(app.find(`.place-card`)).toHaveLength(offers.length);
   });
 });
