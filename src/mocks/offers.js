8bbe8382af89bb85c4b6f1df3807041593118daf
Bulat Mendagaziev <bm@idaproject.com>
2019-12-04 00:44:24 +0300

    feature(offers): add mocks data

---
+++ src/mocks/offers.js
@@ -0,0 +1,34 @@
+export const offers = [
+  {
+    image: `http://placehold.it/260x200`,
+    isPremium: true,
+    cost: 120,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `apartment`,
+    rating: 4.4,
+  },
+  {
+    image: `http://placehold.it/260x200`,
+    isPremium: false,
+    cost: 40,
+    title: `Wood and stone place`,
+    type: `room`,
+    rating: 2.6,
+  },
+  {
+    image: `http://placehold.it/260x200`,
+    isPremium: true,
+    cost: 160,
+    title: `Canal View Prinsengracht`,
+    type: `house`,
+    rating: 4.8,
+  },
+  {
+    image: `http://placehold.it/260x200`,
+    isPremium: false,
+    cost: 80,
+    title: `Nice, cozy, warm big bed`,
+    type: `hotel`,
+    rating: 3.5,
+  },
+];
